53dfd07bd9b26966b321fa74896d17eaa28a6f60
satya_77 <168112840+satya77-lang@users.noreply.github.com>
2026-02-28 12:36:21 +0530

    Comment out GPG verification for Mozilla key
    Comment out the GPG verification command for the Mozilla APT repository key.

--- install.sh
+++ install.sh
@@ -261,7 +261,7 @@ install_firefox() {
   wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc >/dev/null
 
   # 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3
-  gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+  #gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
 
   # Next, add the Mozilla APT repository to your sources.list
   cat <<EOF | sudo tee /etc/apt/sources.list.d/mozilla.sources
