5bea391e5821c138aceefe72aa6eaac316ea8652
satya77-lang <rp6502293@gmail.com>
2026-03-03 11:35:39 +0530

    feat: Update Communications function to improve user selection options

--- install.sh
+++ install.sh
@@ -583,7 +583,7 @@ MultimediaTools
 
 function Communications {
   local selected
-  selected=$(gum choose --header 'Select the Communications for installation' --no-limit 'Discord Telegram Signal Slack Thunderbird Zoom')
+  selected=$(gum choose --header 'Select the Communications for installation' --no-limit 'Discord' 'Telegram' 'Signal' 'Slack' 'Thunderbird' 'Zoom')
   gum style --border double --border-foreground 212 --padding "2 4" "You selected:" "$selected"
 
   while IFS= read -r talk <&3; do
